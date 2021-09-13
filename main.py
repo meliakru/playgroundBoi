d4ba74ec3e790884a34340e6fd183c7928fcc2e9
melkrumm <melkrumm@uiowa.edu>
2021-09-13 16:33:31 -0500

    "another Update"

--- main.py
+++ main.py
@@ -1,15 +1,12 @@
 #This is a playground! It contains fun stuff!
-def aProgram(afile):
+def arogram(afile):
     theFile = open(afile, "r+")
     count = 0
     lineTwo = "this is line 2"
     for line in theFile:
         #Don't forget to strip the file input in order to make comparisons
-        aString = line.strip()
-        if aString == lineTwo:
-            line = "UPDATE".join(line)
-            theFile.write(line)
-        count = count + 1`
+        print(line)
     theFile.close()
 
-aProgram("Example.txt")
+if __name__ == "arogram":
+    arogram("Example.txt")
